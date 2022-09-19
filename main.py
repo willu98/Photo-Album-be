ab9a01911c64fc6c0c4d655d13a1afd0d95d7bde
willu <william.pu98@gmail.com>
2022-09-18 21:41:41 -0400

    enable cors

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 import fastapi as fastapi
+from fastapi.middleware.cors import CORSMiddleware
 
-from routers import user_router
-from routers import pictures_router
+from routers import user_router, pictures_router
 app = fastapi.FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 app.include_router(user_router.user_router, prefix="/user")
 app.include_router(pictures_router.pictures_router, prefix="/photos")
