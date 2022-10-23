13a3fa9329cb87a1a6b35708595bf4857e757d1d
William <willu@my.yorku.ca>
2022-10-23 13:04:06 -0400

    rewind time

--- routers/pictures_router.py
+++ routers/pictures_router.py
@@ -45,12 +45,11 @@ async def get_photo(
 
 @pictures_router.post("/")
 async def upload_photo(
-    photo: schemas.Photo_Submission,
+    file: fastapi.UploadFile,
+    user_filename: str,
     username=fastapi.Depends(auth_handler.auth_wrapper),
     db: orm.Session = fastapi.Depends(services.get_db)
 ):
-    file = photo.file
-    user_filename = photo.user_filename
     fileExtension = re.search(".[0-9a-z]+$", file.filename, re.IGNORECASE)
     print(fileExtension.group(0))
     if fileExtension is None:
